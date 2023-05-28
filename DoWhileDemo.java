7a6806a96cbd638e638ae49a2b579601b3070883
Akshay Narawade <etech5458@gmail.com>
2023-05-28 20:30:23 +0530

    Push Do While Loop

---
+++ DoWhileDemo.java
@@ -0,0 +1,9 @@
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int i = 4;
+        do {
+            System.out.println("Hello");
+            i++;
+        }while(i<=3);
+    }
+}
