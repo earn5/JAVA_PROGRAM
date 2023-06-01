f796d05af62a517f3822e58721c5a451a0f50af4
Akshay Narawade <etech5458@gmail.com>
2023-06-01 06:08:34 +0530

    Loop Demo

--- Loop.java
+++ Loop.java
@@ -2,7 +2,8 @@ public class Loop {
 
     public static void main(String[] args) {
         int i = 1;
-        while(i <= 4) {
+        while(i <= 4
+        ) {
             System.out.println("Hi " + i);
             int j = 1;
             while(j <= 3) {
